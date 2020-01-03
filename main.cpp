18fd1d878362f56e439609062e78b69279722927
Michal Labowski <michal.labowski@gmail.com>
2020-01-03 20:08:27 +0100

    feat: Add consumer and producer threads

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <thread>
+#include <chrono> /// for sleep_for
 #include <gtest/gtest.h>
 #include "blockingqueue.h"
 
@@ -17,6 +18,31 @@ class A {
 public:
     int* ptr;
 };
+
+void producer(BlockingQueue<std::unique_ptr<A>>& q)
+{
+    int i = 0;
+    while (true) {
+        std::unique_ptr<A> a_ptr = std::make_unique<A>();
+        a_ptr->ptr[0] = i++;
+        std::cout << "[Producer] push data to queue" << std::endl;
+        q.push(std::move(a_ptr));
+        std::cout << "[Producer] sleeping" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+}
+
+void consumer(BlockingQueue<std::unique_ptr<A>>& q)
+{
+    int i = 0;
+    while (true) {
+        std::unique_ptr<A> a_ptr = std::make_unique<A>();
+        std::cout << "[Consumer] Waits for data" << std::endl;
+        q.pop(a_ptr);
+        std::cout << "[Producer] Get data" << std::endl;
+        a_ptr.reset();
+    }
+}
 
 //#define RUN_TESTS
 int main(int argc, char** argv)
